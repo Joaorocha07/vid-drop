7b8a4be244aeeb4cd2ed20eb6033d777f855211f
João Victor Rocha Santos <joao.santos@unitri.edu.br>
2024-08-08 13:43:14 -0300

    alteração teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
-import React, { useEffect } from 'react'
+import { Link } from '@mui/material'
+// import { useRouter } from 'next/navigation'
+import React from 'react'
 
 export default function Home (): JSX.Element {
-  const router = useRouter()
+  // const router = useRouter()
 
-  useEffect(() => {
-    router.push('/tik-tok-video')
-  }, [router])
+  // useEffect(() => {
+  //   router.push('/tik-tok-video')
+  // }, [router])
 
   return (
-    <></>
+    <>
+      <h1>Página principal</h1>
+      <Link href='/tik-tok-video'>acesse aqui o site</Link>
+    </>
   )
 }
